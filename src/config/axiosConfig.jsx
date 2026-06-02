544e858088e1a2671b6a8785a8bd89f71883a953
SufiyaaanShaikh <sufisk2526@gmail.com>
2026-06-02 18:37:43 +0530

    backend endpoint switch to local to server

--- src/config/axiosConfig.jsx
+++ src/config/axiosConfig.jsx
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 
 // Create API instance with base configuration
 const api = axios.create({
-  baseURL: "http://localhost:5017/api",
+  baseURL: "https://explore-mates-backend.vercel.app/api",
   timeout: 10000,
   withCredentials: true,
 });
