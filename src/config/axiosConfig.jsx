49abfb87c78bb27a1e25aa9b408e7d8f0d97c8a0
SufiyaaanShaikh <sufisk2526@gmail.com>
2026-06-02 20:29:49 +0530

    Disable CI warnings

--- src/config/axiosConfig.jsx
+++ src/config/axiosConfig.jsx
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 
 // Create API instance with base configuration
 const api = axios.create({
-  baseURL: "https://explore-mates-backend.vercel.app/api",
+  baseURL: "http://localhost:5017/api",
   timeout: 10000,
   withCredentials: true,
 });
